316575d5674450f82f516ed472bcb5ddc28d37fd
Timothy Overly <timothy.overly@gmail.com>
2016-01-20 13:49:34 -0500

    Working copy

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ember-cli-post-build-copy
-Allow you to copy files external after builds in ember cli
+# EmberCLI Post Build Copy
+
+Once your build is complete you might need to move your build files to an external location
+
+## Installation
+
+* `ember install ember-cli-post-build-copy`
+
+## Configuration
+
+    //config/environment.js
+    ENV['ember-cli-post-build-copy'] = {
+      "development" : [
+          ['/assets/vendor.js','../javascripts/vendor.js']
+      ]
+    }
+
+The first argument is relative to the "dist" folder
+The second argument is relative to the "root: of the ember application
---
+++ index.js
@@ -0,0 +1,45 @@
+/* jshint node: true */
+'use strict';
+
+var path = require('path');
+var fs = require('fs.extra');
+
+module.exports = {
+  name: 'ember-cli-post-build-copy',
+
+  configObject: {},
+  buildEnv: "",
+
+  postBuild: function (results) {
+      console.log("ember-cli-post-build-copy: copying from "+results.directory+".");
+      var self = this;
+      if(self.configObject && self.configObject["ember-cli-post-build-copy"]){
+        let copyConfig = self.configObject["ember-cli-post-build-copy"];
+        if(copyConfig[self.buildEnv]){
+          let copyEnvConfig = copyConfig[self.buildEnv];
+          console.log("ember-cli-post-build-copy: copying files "+JSON.stringify(copyEnvConfig)+".");
+          for (var i = 0; i < copyEnvConfig.length; i++) {
+              let pair = copyEnvConfig[i]
+              let src = path.resolve(results.directory+pair[0])
+              let dest = path.resolve(pair[1])
+              console.log("ember-cli-post-build-copy: copying "+src+" to "+dest);
+              fs.copy(results.directory+pair[0], pair[1]);
+          }
+        }else{
+          console.log("ember-cli-post-build-copy: no copy config for "+this.buildEnv)
+        }
+      }else{
+        console.log("ember-cli-post-build-copy: no ENV['ember-cli-post-build-copy']");
+      }
+  },
+
+  config: function (env, baseConfig) {
+
+    if(this.buildEnv === ""){
+      console.log("ember-cli-post-build-copy: build env set to "+env)
+      this.buildEnv = env;
+      this.configObject = baseConfig;
+    }
+
+  }
+};
